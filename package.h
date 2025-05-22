764b85f17cb8b75e0b7208f2ff931170a5642e6d
João Gabriel Santos Silva <145305652+uJotaQ@users.noreply.github.com>
2025-05-22 11:35:46 -0300

    Adicionando arquivos pra conversão

--- package.h
+++ package.h
@@ -3,13 +3,9 @@
 
 #include <stdint.h>
 
-// Funções em Assembly
-extern void enviar_dados_para_fpga(volatile uint32_t *LEDR_ptr, uint8_t matrizA[5][5], uint8_t matrizB[5][5], uint8_t data);
-extern void receber_dados_da_fpga(volatile uint32_t *LEDR_ptr, uint8_t matrizC[5][5]);
-extern void imprimir_matriz_resultado(uint8_t matrizC[5][5]);
+void enviar_dados_para_fpga(volatile uint32_t *LEDR_ptr, uint8_t matrizA[5][5], uint8_t matrizB[5][5], uint8_t data);
+void receber_dados_da_fpga(volatile uint32_t *LEDR_ptr, uint8_t matrizC[5][5]);
 
-// Funções em C (auxiliares)
-void print_progress_bar(int current, int total);
-void* configurar_mapeamento(int *fd);
+void print_progress_bar(int current, int total);  // Se essa função estiver em outro .c, inclua o .h correto
 
 #endif
